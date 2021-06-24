c5c6cbd14096bea18f3bdf2bf6735fa1a7add0d9
FugacityEngr <61220814+fugacityengr@users.noreply.github.com>
2021-06-24 15:18:02 +0800

    Update myApp.js
    Update getHandler to serve an html file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var app = express();
 
-console.log("Hello World");
+// console.log("Hello World");
+
+// const getHandler = (req, res) => {
+//   res.send("Hello Express");
+// };
+// app.get("/", getHandler);
 
 const getHandler = (req, res) => {
-  res.send("Hello Express");
+  const file = __dirname + /views/index.html;
+  res.send(file);
 };
 app.get("/", getHandler);
 
